19ab475510a4b2d7d2cefb34492b5691508c4c71
vninq <abeekud26@gmail.com>
2026-08-09 23:15:41 -0400

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -3,11 +3,12 @@ using BepInEx;
 
 namespace SceneLoader
     
-    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
-    {
-        private void Awake()
-        {
-            Logger.LogInfo("Plugin SceneLoader is loaded!!!");
-        }
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+  public class Plugin : BaseUnityPlugin
+  {
+      private void Awake()
+     {
+         Logger.LogInfo("Plugin SceneLoader is loaded!!!");
+      }
+      
     }
