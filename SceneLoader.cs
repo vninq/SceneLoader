cc1d492a8967f515dea726ede6724e7b87397b8b
vninq <abeekud26@gmail.com>
2026-08-09 23:18:06 -0400

    Update SceneLoader.cs

--- SceneLoader.cs
+++ SceneLoader.cs
@@ -8,34 +8,35 @@ using lstwoMODS_Core.UI.Elements;
 using lstwoMODS_WobblyLife.UI.TabMenus;
 
 namespace SceneLoader
-{
-    public class SceneLoader : BaseMod
-    {
-        public override string Name => "Scene Loader";
-        public override string Description => "instantly load scenes";
-        public override ModsWindow ModsWindow => lstwoMODS_WobblyLife.Plugin.ServerModsWindow;
 
-        public override Container BuildPanel(string id)
-        {
-            return new Container(id,
+public class SceneLoader : BaseMod
+ {
+      public override string Name => "Scene Loader";
+     public override string Description => "instantly load scenes";
+      public override ModsWindow ModsWindow => lstwoMODS_WobblyLife.Plugin.ServerModsWindow;
 
-               new Button("WobblyIsland", () => LoadS(LoadScene.WobblyIsland)
-               ).WithContentWidth(),
-               new Button("Arcade_Lobby", () => LoadS(LoadScene.Arcade_Lobby)
-               ).WithContentWidth(),
-               new Button("MainMenu", () => LoadS(LoadScene.MainMenu)
-               ).WithContentWidth(),
-               new Button("Space", () => LoadS(LoadScene.Space)
-               ).WithContentWidth(),
-               new Button("Invalid", () => LoadS(LoadScene.Invaild)
-               ).WithContentWidth()
+      public override Container BuildPanel(string id)
+      {
+          return new Container(id,
+
+             new Button("WobblyIsland", () => LoadS(LoadScene.WobblyIsland)
+             ).WithContentWidth(),
+             new Button("Arcade_Lobby", () => LoadS(LoadScene.Arcade_Lobby)
+             ).WithContentWidth(),
+             new Button("MainMenu", () => LoadS(LoadScene.MainMenu)
+             ).WithContentWidth(),
+             new Button("Space", () => LoadS(LoadScene.Space)
+             ).WithContentWidth(),
+             new Button("Invalid", () => LoadS(LoadScene.Invaild)
+            ).WithContentWidth()
 
             );
         }
 
-        private void LoadS(LoadScene scene)
-        {
-            GameInstance.Instance?.Load(scene, null);
-        }
+      private void LoadS(LoadScene scene)
+      {
+          GameInstance.Instance?.Load(scene, null);
+      }
+      
     }
 }
