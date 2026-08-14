4c38f78ebf156e127f6d3084ebb92fe657347580
vninq <abeekud26@gmail.com>
2026-08-14 13:46:46 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BepInEx;
+
+namespace SceneLoader
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
+        }
+    }
+}
