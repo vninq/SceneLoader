6bf352cc6aa4293b5cde161a1dcfc75f4f4eba11
vninq <abeekud26@gmail.com>
2026-08-14 13:48:15 -0400

    i do not have c# 9

--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using BepInEx;
 
-namespace SceneLoader
-{
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+namespace SceneLoader;
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+ {
+    private void Awake()
     {
-        private void Awake()
-        {
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
-        }
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
     }
 }
